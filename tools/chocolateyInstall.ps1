46c69208de08469a9e40fb4ad769672d607453c6
Andy Li <andy@onthewings.net>
2016-08-13 01:47:50 +0800

    updated to nasm 2.12.02, added checksums

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,1 +1,5 @@
-Install-ChocolateyPackage 'nasm' 'exe' '/S' 'http://www.nasm.us/pub/nasm/releasebuilds/2.12.01/win32/nasm-2.12.01-installer-x86.exe' 'http://www.nasm.us/pub/nasm/releasebuilds/2.12.01/win64/nasm-2.12.01-installer-x64.exe'
+Install-ChocolateyPackage 'nasm' 'exe' '/S' `
+ 'http://www.nasm.us/pub/nasm/releasebuilds/2.12.02/win32/nasm-2.12.02-installer-x86.exe' `
+ 'http://www.nasm.us/pub/nasm/releasebuilds/2.12.02/win64/nasm-2.12.02-installer-x64.exe' `
+ -Checksum   'ADD07F2A04197966037D49B9989C17E079EBCD63BD6AD13B0C20FA5CACBAC0AB' -ChecksumType   'sha256' `
+ -Checksum64 'B5FE7A6CB9287FED3A43A19EAE5BAD522398D3E35F9DC958CC0464FE7CA37CC2' -ChecksumType64 'sha256'
