a48463904bed8fc46f1a02527bd6672048f3e97a
Andy Li <andy@onthewings.net>
2018-03-05 14:41:14 +0800

    update to 2.13.03

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 Install-ChocolateyPackage 'nasm' 'exe' '/S' `
- 'http://www.nasm.us/pub/nasm/releasebuilds/2.13.02/win32/nasm-2.13.02-installer-x86.exe' `
- 'http://www.nasm.us/pub/nasm/releasebuilds/2.13.02/win64/nasm-2.13.02-installer-x64.exe' `
- -Checksum   '68b83b961336848bfc241e98892f5e2171906338e5f73d0c6d5e01b43b36a702' -ChecksumType   'sha256' `
- -Checksum64 '1097b4b088ed269308ff1724a5ccaf7ecf3a68654786c8ca1f0ba0b5ae8a6dbc' -ChecksumType64 'sha256'
+ 'http://www.nasm.us/pub/nasm/releasebuilds/2.13.03/win32/nasm-2.13.03-installer-x86.exe' `
+ 'http://www.nasm.us/pub/nasm/releasebuilds/2.13.03/win64/nasm-2.13.03-installer-x64.exe' `
+ -Checksum   'ef5991c04b762b5f8572c913c2810d6b67561944d4e91031fb7dc6fda455a335' -ChecksumType   'sha256' `
+ -Checksum64 'c80a6dde9e5017f373ead8510fa1bd672cc1141ee93e617a85cca3a457902a43' -ChecksumType64 'sha256'
