9cb2ac28e358b7eeb79ab380dd5d7554224475a4
Bryce Glover <RandomDSdevel@gmail.com>
2021-09-11 19:17:58 -0400

    Update nasm to v2.15.05.
         Also update package sources to reflect upstream's long-completed move to GitHub.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 Install-ChocolateyPackage 'nasm' 'exe' '/S' `
- 'https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/win32/nasm-2.14.02-installer-x86.exe' `
- 'https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/win64/nasm-2.14.02-installer-x64.exe' `
- -Checksum   '9e994909e19aec6df83265673541f426d1a80246b996f2043c5d5663bacf4306' -ChecksumType   'sha256' `
- -Checksum64 '14527316cc1b7522bf0c59c328eb9ba16c172d6386feff3867cba9cf23b7db5b' -ChecksumType64 'sha256'
+ 'https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win32/nasm-2.15.05-installer-x86.exe' `
+ 'https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win64/nasm-2.15.05-installer-x64.exe' `
+ -Checksum   '576dcd994f6229d18057f16ef9c7a7c830a148dd83dadaa5a8d83a8e0397eed2' -ChecksumType   'sha256' `
+ -Checksum64 'a02325b9fe54f917f5d6a3036637b38dbb6addf6f7ba9d344d9b943a143fe7d0' -ChecksumType64 'sha256'
